579ad5e358f99ca140450967eba116fb75e8b7f0
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2025-03-17 20:33:02 +0530

    added req.txt

--- final_imagetextoverlay.py
+++ final_imagetextoverlay.py
@@ -105,5 +105,8 @@ def get_transcripts():
 
     return jsonify(results)
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5001, debug=True)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run()
